10bb98417c5a5ce41f5c73ee2d01469663132eb1
Jose Neto <neto@hada5.com>
2015-06-22 10:11:56 -0300

    state in views and stories

--- samples/login/src/login.jsx
+++ samples/login/src/login.jsx
@@ -3,17 +3,17 @@ import FluxEasy from 'flux-easy';
 class LoginStore extends FluxEasy.Store {
 
     constructor() {
-        this.logged_user = null;
+        this.state.logged_user = null;
         this.checkWindowLocationHash();
     }
 
     getLoggedUser() {
-        return this.logged_user;
+        return this.state.logged_user;
     }
 
     checkWindowLocationHash() {
         if (window.location.hash) {
-            this.logged_user = window.location.hash;
+            this.state.logged_user = window.location.hash;
             this.emit('LoggedIn');
         }
     }
@@ -21,11 +21,11 @@ class LoginStore extends FluxEasy.Store {
     login(name, password) {
          setTimeout(function(){
             this.emit("bla");
-            this.logged_user = null;
+            this.state.logged_user = null;
 
         }.bind(this), 1);
         if (name!='' && password == '123') {
-            this.logged_user = name;
+            this.state.logged_user = name;
             this.emit('LoggedIn', {
                 name: name
             })
@@ -34,7 +34,7 @@ class LoginStore extends FluxEasy.Store {
     }
 
     logout() {
-        this.logged_user = null;
+        this.state.logged_user = null;
         this.emit('LoggedOut')
     }
 }
@@ -48,11 +48,15 @@ class LoginStore extends FluxEasy.Store {
       <button onClick={this.login}>Login</button>
   </div>
   <script>
-    this.username='';
-    this.password='123';
-    this.loginStore= LoginStore.createStoreReference();
-    this.loginStore.addEventListener('LoggedIn', this);
-    this.loginStore.addEventListener('LoggedOut', this);
+    {
+    function constructor(){
+      this.state.username='';
+      this.state.password='123';
+      LoginView.loginStore= LoginStore.createStoreReference();
+      LoginView.loginStore.addEventListener('LoggedIn', this);
+      LoginView.loginStore.addEventListener('LoggedOut', this);
+      }
+    }
   </script>
 </LoginView>
 
